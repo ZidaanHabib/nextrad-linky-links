18555ffcf28c627776b91eb66f5a30cd2a13a711
Zidaan <zizouhabib@gmail.com>
2021-10-02 19:24:46 +0200

    Added motor controller serial interface with a send_command() method

---
+++ motor_controller_serial_client.py
@@ -0,0 +1,23 @@
+import serial
+
+
+class ControllerSerialClient:
+
+    def __init__(self):
+        self._serial_connection = serial.Serial(port='/dev/ttyUSB0',
+                                                    baudrate=9600,
+                                                    parity=serial.PARITY_NONE,
+                                                    stopbits=serial.STOPBITS_ONE)
+
+    def send_command(self, axis: int, cmd_char: str, cmd_data: str ) -> None:
+        """ Method to send serial cmmand to motor controller"""
+
+        cmd_list: [str] = [":"]  # all commands start with ':'
+        cmd_list.append(cmd_char)
+        cmd_list.append(str(axis))  # 1: azimuth axis, 2: elevation axis
+        cmd_list.append(cmd_data)  #  depending on command, additional bytes may need to be sent
+        cmd_list.append("\r")  # carriage return stop byte
+
+        cmd = cmd_list.join()
+        self._serial_connection.write(cmd.encode())
+
