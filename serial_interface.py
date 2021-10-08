bd7d0a006965cfda23e0ef3e5ca168943ffbdedc
Zidaan <hbbzid001@myuct.ac.za>
2021-10-08 13:38:39 +0200

    Added stop and set tracking command

--- serial_interface.py
+++ serial_interface.py
@@ -1,4 +1,5 @@
 import serial
+import time
 
 
 class SerialInterface:
@@ -14,9 +15,29 @@ class SerialInterface:
         msg: str = "P" + chr(2) + chr(16) + chr(36) + chr(self.rate) + chr(0)*3
         self.serial.write(msg.encode())
 
+    def move_negative(self):
+        msg: str = "P" + chr(2) + chr(16) + chr(37) + chr(self.rate) + chr(0)*3
+        self.serial.write(msg.encode())
+
+    def stop_slew(self):
+        msg = "P" + chr(2) + chr(16) + chr(36) + chr(0) + chr(0) * 3
+        self.serial.write(msg.encode())
+
+    def set_tracking(self, mode):
+        msg = "T" + str(mode)
+        self.serial.write(msg.encode())
+
+
+
+
 
 if __name__ == "__main__":
     sc = SerialInterface()
-    sc.move_positive()
+    sc.set_tracking(0)
+    sc.move_negative()
     response = sc.serial.read(1)
     print(response.decode('utf-8'))
+    time.sleep(5)
+    sc.stop_slew()
+    print("Stopped")
+    sc.serial.close()
