dc64e57fe9a32b7738f1ff9120e136f40e52a414
Zidaan <zizouhabib@gmail.com>
2021-10-12 12:49:08 +0200

    Added config file and controller_init() method to read config file

--- clients/pedestal_controller.py
+++ clients/pedestal_controller.py
@@ -3,6 +3,7 @@ from dtypes.gps_location import GPSLocation
 from clients.hand_controller_serial_client import SynscanSerialClient
 from interfaces.serial_interface import SerialInterface
 from configparser import ConfigParser
+import os
 
 
 class PedestalController:
@@ -24,22 +25,31 @@ class PedestalController:
         self._el_limits = [-1, -1]
         self._slew_rate_limit = 100000
 
+    def controller_init(self) -> None:
+        self._cf = ConfigParser("config.ini")
+        self._az_limits = [self._cf["Constraints"]["MinAzimuth"], self._cf["Constraints"]["MaxAzimuth"]]
+        self._el_limits = [self._cf["Constraints"]["MinElevation"], self._cf["Constraints"]["MaxElevation"]]
+        self._slew_rate_limit = self._cf["Constraints"]["MaxSlewRate"]
+
     """Setter methods: """
     def set_location(self, latitude: float, lat_dir: chr, longitude: float, long_dir: chr) -> None:
         """ Manually set location if GPS device not funcitonning correctly"""
         self._location = GPSLocation(latitude, lat_dir, longitude, long_dir)
 
-    def set_altitude(self, altitude: float):
+    def set_altitude(self, altitude: float) -> None:
         """ Manually set altitude if GPS device not functioning correctly"""
         self._altitude = altitude
 
-    def set_az_limits(self, az_limit: [float]):
+    def set_az_limits(self, az_limit: [float]) -> None:
+        """ Set azimuth limits for pedestal"""
         self._az_limits = az_limit
 
-    def set_el_limits(self, el_limits: [float]):
+    def set_el_limits(self, el_limits: [float]) -> None:
+        """ Set elevation limits for pedestal"""
         self._el_limits = el_limits
 
-    def set_slew_rate_limit(self, limit: float):
+    def set_slew_rate_limit(self, limit: float) -> None:
+        """ Set slew rate limits for pedestal"""
         self._slew_rate_limit = limit
 
     """Getter methods:"""
@@ -52,4 +62,8 @@ class PedestalController:
     def get_elevation(self):
         return self._el_current
 
+
+if __name__ == "__main__":
+    print("hello")
+    print(os.getcwd())
 
