370f591e43d41872a03284702b15760b106e732c
Zidaan <zidaanhabib06@gmail.com>
2021-10-28 15:30:49 +0200

    Added method to instantiate a fake AZEQ6 pedestal device

--- remotes/azeq6_remote.py
+++ remotes/azeq6_remote.py
@@ -3,9 +3,17 @@ from interfaces.pedestal_device_interface import IPedestalDevice
 from clients.hand_controller_serial_client import SynscanSerialClient
 from clients.gps_client import GPSClient
 from clients.serial_connection import SerialConnection
+#  fake clients for testing purposes
+from clients.fake_gps_client import FakeGPSClient
+from clients.fake_controller_client import FakeControllerClient
+
 
 class AZEQ6PedestalRemote:
 
     @staticmethod
     def get_pedestal_device() -> IPedestalDevice:
         return AZEQ6Pedestal(SynscanSerialClient(SerialConnection()), GPSClient())
+
+    @staticmethod
+    def get_fake_pedestal_device() -> IPedestalDevice:
+        return AZEQ6Pedestal(FakeControllerClient(), FakeGPSClient())
