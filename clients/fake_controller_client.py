bc93c3fe321be7ac6c529b1e08b4f35328c6f8fb
Zidaan <zidaanhabib06@gmail.com>
2021-10-28 15:30:12 +0200

    Added ovverride methods from IControllerInterface

--- clients/fake_controller_client.py
+++ clients/fake_controller_client.py
@@ -1,35 +1,41 @@
-from interfaces.connection_interface import ConnectionInterface
+from interfaces.controller_interface import IControllerInterface
 from time import sleep
 import random
 
 
-class FakeControllerClient():
+class FakeControllerClient(IControllerInterface):
     """Fake controller used for development and testing purposes"""
 
     def __init__(self):
         pass
+    def calibrate_command(self):
+        print("calibrating")  # manufacturer command to set current elevation as 0
 
-    def slew_positive_fixed(self, axis, slew_rate_preset):  #axis == 1: azimuth, 2: elevation
-        print("< Slew positive fixed> ")
+    def goto_az_el(self, azimuth: float, elevation: float):
+        print("goto command")
 
-    def slew_negative_fixed(self, axis: int, slew_rate_preset: int):
-        print("< Slew negative fixed> ")
+    def is_goto(self):
+        print("is goto in action ")
 
-    #def slew_positive_variable(self, rate: float ):
+    def slew_positive_fixed(self, axis, preset):  #axis == 1: azimuth, 2: elevation
+        print("slew positive fixed command")
 
+    def slew_negative_fixed(self, axis: int, preset: int):
+        print("slew negative fixed command")
+
+    def slew_positive_specific(self, axis: chr, slew_rate: float, azimuth_diff, elevation_diff ):  # rate is in arcseconds per sec
+        print("slew positive specific")
+
+
+    def slew_negative_specific(self, axis: chr, slew_rate: float,  azimuth_diff, elevation_diff ):  # rate is in arcseconds per sec
+        print("stop negative specific")
 
     def stop_slew(self, axis):
-        print("<Stopped slewing>")
-
-    def set_tracking(self, mode):
-        print("<Set tracking mode to >" + mode)
+        print("stop slew command")
 
 
-    def get_azimuth(self) -> float:
-        az = random.uniform(180, 360)
-        return az
+    def get_azimuth(self) -> str:
+        print("get azimuth")
 
-    def get_elevation(self):
-        el = random.uniform(180, 360)
-        return el
-
+    def get_elevation(self) -> str:
+        print("get elevation")
