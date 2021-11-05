fc459dc576fdcfed5c12186d2a013744355af1bb
Zidaan <zidaanhabib06@gmail.com>
2021-11-05 16:43:15 +0200

    Added doc strings

--- clients/fake_controller_client.py
+++ clients/fake_controller_client.py
@@ -8,34 +8,47 @@ class FakeControllerClient(IControllerInterface):
 
     def __init__(self):
         pass
+
     def calibrate_command(self):
-        print("calibrating")  # manufacturer command to set current elevation as 0
+        """ Fake calibration command """
+
+        print(" Fake calibrating command from FakeControllerClient")  # manufacturer command to set current elevation as 0
 
     def goto_az_el(self, azimuth: float, elevation: float):
-        print("going to Az:{} , El:{}".format(azimuth, elevation))
+        """ Fake goto command"""
+        print("going to Az:{} , El:{} from FakeControllerClient".format(azimuth, elevation))
 
     def is_goto(self):
-        print("is goto in action ")
+        """ Fake is_goto command """
+
+        print("Fake is_goto command from FakeControllerClient ")
 
     def slew_fixed(self, axis, preset, dir):  #axis == 1: azimuth, 2: elevation
         print("slew positive fixed command")
 
-
     def slew(self, axis: chr, slew_rate: float, dir: chr):  # rate is in arcseconds per sec
-        print("slewing positive specific")
+        """ Fake slew command """
+
+        print("Fake slew command from FakeControllerClient")
 
     def stop_slew(self, axis):
-        print("Stop slew")
+        """ Fake stop command """
+        print("Fake stop slew command from FakeControllerClient")
 
     def slew_step(self, axis, direction, slew_rate):
+        """ """
         print("Slewing 1 degree")
 
 
     def get_azimuth(self) :
-        print("get azimuth")
-        return "000000"
+        """ Fake get_azimuth command """
+
+        print("fake get azimuth command from FakeControllerClient")
+        return "000000"  # return dummy value
 
 
     def get_elevation(self) :
-        print("get elevation")
+        """ Fake get_elevation command"""
+
+        print("Fake get elevation command from FakeControllerClient")
         return "000000"
